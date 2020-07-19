e3271c68aff37543d9b6a767cde9eeeb027212d2
first-phoenix <iswarajkhan@gmail.com>
2020-07-19 10:52:49 +0530

    adding another python file for sending media as well as msgs

--- wa_spam_msg+media.py
+++ wa_spam_msg+media.py
@@ -3,30 +3,55 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://web.whatsapp.com/')
+driver.get("https://web.whatsapp.com/")
+driver.maximize_window()
 
-name = input('Enter the name of user or group : ')
-filepath = input('Enter your filepath (images/video): ')
-count = int(input("Enter count:"))
+msgtype = input("Which type of message you want to send? 1.Text 2.Media: Select 1 or 2 :")
 
-input('Enter anything after scanning QR code')
+def txtmsg():
+    name = input("Enter name or group name:")
+    msg = input("Enter message:")
+    count = int(input("Enter count:"))
 
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
+    input("Enter anything after scanning QR code:")
 
-for index in range(count):
-    attachment_box = driver.find_element_by_xpath('//div[@title = "Attach"]')
-    attachment_box.click()
+    user = driver.find_element_by_xpath("//span[@title='{}']".format(name))
+    user.click()
 
+    msg_box = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
 
-    image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
-    image_box.send_keys(filepath)
+    for index in range(count):
+        msg_box.send_keys(msg)
+        driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
 
-    sleep(3)
-
-    send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
-    send_button.click()
-
-print("Success")
+    print("Success")
 
 
+
+def mmsmsg():
+    name = input('Enter the name of user or group : ')
+    filepath = input('Enter your filepath (images/video): ')
+    count = int(input("Enter count:"))
+
+    input('Enter anything after scanning QR code')
+
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
+
+    for index in range(count):
+        attachment_box = driver.find_element_by_xpath('//div[@title = "Attach"]')
+        attachment_box.click()
+        image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+        image_box.send_keys(filepath)
+        sleep(3)
+        send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
+        send_button.click()
+
+    print("Success")
+
+if msgtype=="1":
+    txtmsg()
+elif msgtype=="2":
+    mmsmsg()
+else:
+    print("Wrong Input.")
